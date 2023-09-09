47946ec1dad6839b65862eaaf4fdc7b5ce6b540d
Shamruddin <124250239+Shamruddin@users.noreply.github.com>
2023-09-09 22:59:51 +0530

    Update k8sss.sh

--- k8sss.sh
+++ k8sss.sh
@@ -153,6 +153,78 @@ sudo install minikube-linux-amd64 /usr/local/bin/minikube
 sudo snap install kubectl --classic
 minikube start --driver=docker
 
+
+
+
+Step 2: Install Required Packages
+Install some basic required packages.
+
+sudo apt install -y curl wget apt-transport-https
+
+Step 3: Install Docker
+Minikube can run a Kubernetes cluster either in a VM or locally via Docker. This guide demonstrates the Docker method.
+
+sudo apt install -y docker.io
+
+
+Start and enable Docker.
+
+sudo systemctl enable --now docker
+Add current user to docker group (To use docker without root)
+
+sudo usermod -aG docker $USER && newgrp docker
+Now, logout (use exit command) and connect again.
+
+
+Step 4: Install Minikube
+First, download the Minikube binary using curl:
+
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+Make it executable and move it into your path:
+
+chmod +x minikube
+sudo mv minikube /usr/local/bin/
+
+Step 5: Install kubectl
+Download kubectl, which is a Kubernetes command-line tool.
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+Check above image ⬆️ Make it executable and move it into your path:
+
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+
+
+Step 6: Start Minikube
+Now, you can start Minikube with the following command:
+
+minikube start --driver=docker
+This command will start a single-node Kubernetes cluster inside a Docker container.
+
+
+Step 7: Check Cluster Status
+Check the cluster status with:
+
+minikube status
+
+
+You can also use kubectl to interact with your cluster:
+
+kubectl get nodes
+
+
+Step 8: Stop Minikube
+When you are done, you can stop the Minikube cluster with:
+
+minikube stop
+
+
+Optional: Delete Minikube Cluster
+If you wish to delete the Minikube cluster entirely, you can do so with:
+
+minikube delete
+
+
 -------calculator app for nginx--------
 https://github.com/sumanprasad007/cicd-on-kubernetes.git
 kubectl port-forward svc/my-service 30007:80 --address 0.0.0.0 &
